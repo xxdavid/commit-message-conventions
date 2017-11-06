6eed40d6aa32482ebeaec13034dbb14397c7cfa2
David Pavlík <david@dpavlik.cz>
2017-11-06 16:52:23 +0100

    Download a JSON archive, extract it, parse, filter and write to a file

---
+++ download-and-parse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import sys
+import os
+import json
+import gzip
+import urllib.request
+import io
+
+date = sys.argv[1]
+
+url = f"http://data.githubarchive.org/{date}.json.gz"
+
+response = urllib.request.urlopen(url)
+compressed_file = io.BytesIO(response.read())
+file = gzip.GzipFile(fileobj=compressed_file)
+
+output_filename = "messages.txt"
+output_mode = "a" if os.path.exists(output_filename) else "w"
+output_file = open(output_filename, output_mode)
+
+with file as f:
+    for line in f:
+        event = json.loads(line)
+        if event['type'] == 'PushEvent':
+            author = event['actor']['login']
+            repo = event['repo']['name']
+            commits = event['payload']['commits']
+            for commit in commits:
+                message_lines = commit['message'].split("\n")
+                output_file.write(f"{author}::{repo}::{len(message_lines)}::{message_lines[0]}\n")

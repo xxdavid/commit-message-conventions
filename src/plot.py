5ff9b8f5a6ac1d36acc04f625512f96c3c3bd63b
David Pavlík <david@dpavlik.cz>
2017-12-02 18:48:53 +0100

    Refactor plot.py by splitting plotters declaration and plotting

--- src/plot.py
+++ src/plot.py
@@ -5,13 +5,18 @@ from os import makedirs
 
 makedirs(Directories.charts, exist_ok=True)
 
-FirstWordFrequencyPlotter().plot()
-WordFrequencyPlotter().plot()
-VerbFormOverviewPlotter().plot()
-ImperativePlotter().plot()
-GerundWordsPlotter().plot()
-ThirdPersonWordsPlotter().plot()
-PastTenseWordsPlotter().plot()
-MessageLengthPlotter().plot()
-MessageLinesPlotter().plot()
-BinaryAnalysesPlotter().plot()
+plotters = [
+    FirstWordFrequencyPlotter(),
+    WordFrequencyPlotter(),
+    VerbFormOverviewPlotter(),
+    ImperativePlotter(),
+    GerundWordsPlotter(),
+    ThirdPersonWordsPlotter(),
+    PastTenseWordsPlotter(),
+    MessageLengthPlotter(),
+    MessageLinesPlotter(),
+    BinaryAnalysesPlotter(),
+]
+
+for plotter in plotters:
+    plotter.plot()

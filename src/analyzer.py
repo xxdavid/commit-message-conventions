6dffc34114f0a2f292edaa638ebf7e30eb579977
David Pavlík <david@dpavlik.cz>
2017-12-29 17:06:22 +0100

    Handle ValueError when analysing the commits

--- src/analyzer.py
+++ src/analyzer.py
@@ -16,15 +16,18 @@ class Analyzer:
         file = open(f"{Directories.processed_data}/commits.txt")
         with file:
             for line in file:
-                [author, repo, lines, message] = line.split("::", 3)
-                if author not in self.authors:
-                    self.authors[author] = 0
-                if self.authors[author] < self.MAX_COMMITS_BY_AUTHOR:
-                    message = message.strip()
-                    for analysis in self.analyses:
-                        analysis.analyze_commit(author, repo, lines, message)
-                    self.authors[author] += 1
-                    self.analyzed_number += 1
+                try:
+                    [author, repo, lines, message] = line.split("::", 3)
+                    if author not in self.authors:
+                        self.authors[author] = 0
+                    if self.authors[author] < self.MAX_COMMITS_BY_AUTHOR:
+                        message = message.strip()
+                        for analysis in self.analyses:
+                            analysis.analyze_commit(author, repo, lines, message)
+                        self.authors[author] += 1
+                        self.analyzed_number += 1
+                except ValueError:
+                    print("ValueError, skipping.")
                 self.total_number += 1
 
         for analysis in self.analyses:
